91cfc3605b991efe2bb6bbbc4d94468fe8b42f10
Jun Hu <hu.jun@iisgalvanimi.edu.it>
2025-02-26 12:25:56 +0000

    strutturato i componeti secondo lo schema

--- Hu-fila-B/src/app/third/third.component.ts
+++ Hu-fila-B/src/app/third/third.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-third',
+  standalone : true,
   imports: [],
   templateUrl: './third.component.html',
   styleUrl: './third.component.css'
